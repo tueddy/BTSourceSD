ff7a27bb1d98dbde582ae170889dcf6dfdf98b43
tueddy <kaffee>
2022-09-17 21:10:36 +0200

    use event audio_process_i2s()

--- BTSourceSD.ino
+++ BTSourceSD.ino
@@ -11,42 +11,41 @@
 #define I2S_BCLK      27
 #define I2S_LRC       26
 
-#define BT_NAME "MI Portable Bluetooth Speaker"
+#define BT_NAME "My POGS Wireless Headphone"
+//#define BT_NAME "MI Portable Bluetooth Speaker"
 
 Audio audio;
 BluetoothA2DPSource a2dp_source;
 RingbufHandle_t audioRingBuffer;
 
-
 // feed the A2DP source with audio data
 int32_t get_data_channels(Frame *frame, int32_t channel_len) {
     if (channel_len < 0 || frame == NULL)
         return 0;
-
     // Receive data from ring buffer
     size_t len{};
-    vRingbufferGetInfo(audioRingBuffer, nullptr, nullptr, nullptr, nullptr, &len);
+    #if ESP_ARDUINO_VERSION_MAJOR >= 2
+        vRingbufferGetInfo(audioRingBuffer, nullptr, nullptr, nullptr, nullptr, &len);
+    #else
+        vRingbufferGetInfo(audioRingBuffer, nullptr, nullptr, nullptr, &len);
+    #endif    
     if (len < (channel_len * 4)) {
-        Serial.println("not enough data");
+        //Serial.println("not enough data");
         return 0;
     };
-
     size_t sampleSize = 0;
     uint8_t* sampleBuff;
     sampleBuff = (uint8_t *)xRingbufferReceiveUpTo(audioRingBuffer, &sampleSize, (portTickType)portMAX_DELAY, channel_len * 4);
     if (sampleBuff != NULL) {
-        if (channel_len  != (sampleSize / 4)) {
-   	        printf( "a2dp_input_data_callback itemSize != dataLength (%u != %u)\n", sampleSize, channel_len );
-        };
         // fill the channel data
         for (int sample = 0; sample < (channel_len); ++sample) {
             frame[sample].channel1 = (sampleBuff[sample * 4 + 3] << 8) | sampleBuff[sample * 4 + 2];
             frame[sample].channel2 = (sampleBuff[sample * 4 + 1] << 8) | sampleBuff[sample * 4];
-        }
+        };
         vRingbufferReturnItem(audioRingBuffer, (void *)sampleBuff);
     };
     return channel_len;   
-}
+};
 
 
 // Bluetooth connection state changed
@@ -65,7 +64,8 @@ void audio_state_changed(esp_a2d_audio_state_t state, void *ptr){
 void setup() {
     Serial.begin(115200);
     Serial.println("");
-    Serial.println("BTSource");
+    Serial.println("BTSource demo");
+    Serial.println("This demo streams audio data to bluetooth headset");
     Serial.println("");
     Serial.print("ESP-IDF version: ");
     Serial.println(String(ESP.getSdkVersion()));
@@ -74,9 +74,11 @@ void setup() {
     //  setuo BT source
     a2dp_source.set_on_connection_state_changed(connection_state_changed);
     a2dp_source.set_on_audio_state_changed(audio_state_changed);
+    a2dp_source.set_auto_reconnect(false);
     a2dp_source.start(BT_NAME, get_data_channels);  
-    a2dp_source.set_volume(15);
-    //  start SD (SPI mode)
+    // a2dp_source.set_task_core(11); /
+    a2dp_source.set_volume(64); // BT-source volume
+    //  start SD-card (SPI mode)
     pinMode(12, OUTPUT);
     pinMode(14, OUTPUT);
     digitalWrite(12, HIGH);
@@ -97,26 +99,29 @@ void loop()
         if (!audio.isRunning()) {
             // start audio playback
             Serial.println("connecttoFS");
-            audio.connecttoFS(SD, "/test.mp3");  //48khz.wav");//"stereo-test.mp3");//
+            audio.connecttoFS(SD, "/test.mp3");  
+//            audio.connecttoFS(SD, "/Häschen_Hüpf.mp3");  
+//            audio.connecttoFS(SD, "/Rabe.mp3");  
+//            audio.connecttoFS(SD, "stereo-test.mp3");
         } else {
-        //
+            ;
         }
    } 
 }
 
 // optional
 void audio_info(const char *info){
-    //if (startsWith((char *)info, "SampleRate")) {
-        Serial.print("info        "); Serial.println(info);
-    //}
+    Serial.print("info        "); 
+    Serial.println(info);
 }
 
 
-
-void audio_process_extern(int16_t* buff, uint16_t len, bool *continueI2S){
-    *continueI2S = (!a2dp_source.is_connected());
-    if (len) {
-        // fill ringbuffer with audio data
-        xRingbufferSend(audioRingBuffer, buff, len * 4, (portTickType)portMAX_DELAY);
-    }    
+void audio_process_i2s(uint32_t* sample, bool *continueI2S){
+    if (a2dp_source.is_connected()) {
+        // send audio sample to ringbuffer
+        *continueI2S = (pdTRUE != xRingbufferSend(audioRingBuffer, sample, sizeof(uint32_t), (portTickType)portMAX_DELAY));
+    } else {
+        *continueI2S = true;
+    }
 }
+
